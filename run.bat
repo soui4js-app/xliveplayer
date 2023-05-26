fd1a8275aad8b6e31cbc592bc13c00c805fa95ef
huangjianxiong1 <huangjianxiong1@yy.com>
2023-05-26 11:06:16 +0800

    同步新版本目录结构更改。

--- run.bat
+++ run.bat
@@ -1,12 +1,12 @@
 @ECHO off
 if defined SOUI4JS_PATH (
  echo %SOUI4JS_PATH%
- start %SOUI4JS_PATH%\Release\soui4js-host xliveplayer
+ start %SOUI4JS_PATH%\bin\soui4js-host xliveplayer
  exit
 ) else (
  echo "no path"
- if exist ..\soui4js\Release\soui4js-host.exe (
- 	start ..\soui4js\Release\soui4js-host xliveplayer
+ if exist ..\soui4js\bin\soui4js-host.exe (
+ 	start ..\soui4js\bin\soui4js-host xliveplayer
  	exit
  ) else (
  	echo "没有找到环境变量:SOUI4JS_PATH, 在同级目录也没有找到soui4js, 启动失败"
